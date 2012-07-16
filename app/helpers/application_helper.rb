1ae08336a50cacecf5df3c49c220a1d0c9d79768
Rahul Madan <rahulsachint@gmail.com>
2012-07-16 15:21:01 -0700

    Chapter 5 complete, Site layout & Routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+def title
+  baseTitle = "Ruby on Rails Tutorial Sample App"
+  if @title.nil?
+      baseTitle
+  else 
+      "#{baseTitle} | #{@title}"
+  end     
+end
+
 end
